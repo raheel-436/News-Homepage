0075fe8e7d49eddaf88d8bfd56fb7d2c1359ec74
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-12-28 15:54:37 +0500

    'Responsiveness Added: Navbar for mobiles'

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
+const navLinks = document.querySelector(".nav-links");
 const menuBtn = document.querySelector(".menu-btn");
 const closeMenuBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.style.display = "flex";
+  closeMenuBtn.style.display = "block";
+  menuBtn.style.display = "none";
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  navLinks.style.display = "none";
+  closeMenuBtn.style.display = "none";
+  menuBtn.style.display = "flex";
+});
